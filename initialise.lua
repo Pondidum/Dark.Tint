668ae2f3f92d64d3301a4068eac1d12559ad5404
andy <andy@andydote.co.uk>
2014-03-25 22:18:47 +0000

    initialise: load dependencies into ns.lib

--- initialise.lua
+++ initialise.lua
@@ -1,7 +1,16 @@
 local addon, ns = ...
 
 local initialise = function()
+
+	local core = Dark.core
+
 	ns.features = {}
+
+	ns.lib = {
+		events = core.events,
+		fonts = core.fonts,
+	}
+
 end
 
 initialise()
