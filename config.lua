6b30f6aeffce58d765b566db07426337a86d9e28
andy <andy@andydote.co.uk>
2014-04-01 15:11:04 +0100

    config: import mapping config

--- config.lua
+++ config.lua
@@ -1,5 +1,10 @@
 local addon, ns = ...
 
 ns.config = {
-
+	mapping = {
+		spacing = 5,
+		screenOffset = 10,
+		mailIcon = [[Interface\AddOns\Dark.Mapping\textures\mail]],
+		searchIcon = [[Interface\AddOns\Dark.Mapping\textures\search]],
+	}
 }
