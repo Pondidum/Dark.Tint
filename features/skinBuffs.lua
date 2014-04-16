7ac8140386e43dc2d8fee57b04c062dd2ff17053
andy <andy@andydote.co.uk>
2014-04-16 21:13:15 +0100

    skinBuffs: move out of Dark.Buffs

---
+++ features/skinBuffs.lua
@@ -0,0 +1,46 @@
+local addon, ns = ...
+
+local style = ns.lib.style
+local fonts = ns.lib.fonts
+
+local skinBuffs = function()
+
+	local styleButton = function(name)
+
+		local button = _G[name]
+		local icon = _G[name.."Icon"]
+		local border=  _G[name.."Border"]
+
+		button.duration:SetFont(fonts.normal, 10)
+		style.applyShadowTo(button)
+
+		icon:SetTexCoord(.08, .92, .08, .92)
+		icon:SetPoint("TOPLEFT", button, 2, -2)
+		icon:SetPoint("BOTTOMRIGHT", button, -2, 2)
+
+		if border then
+			border:Hide()
+		end
+
+	end
+
+	hooksecurefunc("BuffFrame_Update", function()
+
+		for i = 1, BUFF_ACTUAL_DISPLAY do
+			styleButton("BuffButton"..i)
+		end
+
+		for i = 1, DEBUFF_ACTUAL_DISPLAY do
+			styleButton("DebuffButton"..i)
+		end
+
+		for i = 1, NUM_TEMP_ENCHANT_FRAMES do
+			styleButton("TempEnchant"..i)
+		end
+
+
+	end)
+
+end
+
+ns.features.skinBuffs = skinBuffs
