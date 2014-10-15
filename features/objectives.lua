624033e0b88d400ca193b09a22db1b8f72caa32f
andy <andy@andydote.co.uk>
2014-10-15 17:55:03 +0100

    objectives: objectiveTrackerFrame instead of WatchFrame

--- features/objectives.lua
+++ features/objectives.lua
@@ -2,26 +2,17 @@ local addon, ns = ...
 
 local moveObjectives = function()
 
-	local frame = WatchFrame
-	local button = WatchFrameCollapseExpandButton
-	local header = WatchFrameHeader
+	local frame = ObjectiveTrackerFrame
 
 	local resolution = select(GetCurrentResolution(), GetScreenResolutions())
 	local height = tonumber(string.match(resolution, "%d+x(%d+)"))
 
 	frame:ClearAllPoints()
-	frame:SetPoint("LEFT", UIParent, "LEFT", 10, 0)
+	frame:SetPoint("LEFT", UIParent, "LEFT", 30, 0)
 	frame:SetHeight(height / 2)
 
 	frame.SetPoint = function() end
 	frame.ClearAllPoints = function() end
-
-	button:ClearAllPoints()
-	button:SetPoint("TOPLEFT")
-
-	header:ClearAllPoints()
-	header:SetPoint("LEFT", button, "RIGHT", 5, -2)
-
 
 end
 
